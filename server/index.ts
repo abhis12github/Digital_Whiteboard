32328be94a93ed648a0fd25d915d2b39f2442844
Abhishek Anand <mailtomeabhi15@gmail.com>
2024-12-28 19:06:52 +0530

    updated package.json and added dotenv

--- server/index.ts
+++ server/index.ts
@@ -6,6 +6,9 @@ import express from "express";
 import next, { NextApiHandler } from "next";
 import { Server } from "socket.io";
 import { v4 } from "uuid";
+import {config} from "dotenv";
+
+config();
 
 const port = parseInt(process.env.PORT || "3000", 10);
 const dev = process.env.NODE_ENV !== "production";
